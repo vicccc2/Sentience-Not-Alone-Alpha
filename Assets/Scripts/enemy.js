a9a41af2515d3cefde391fb306362910a58a4cd4
vicccc2 <167596999+vicccc2@users.noreply.github.com>
2024-05-07 18:09:59 -0700

    Add files via upload

---
+++ Assets/Scripts/enemy.js
@@ -0,0 +1,20 @@
+// enemy class
+class baseEnemies {
+  health;
+  damage;
+  enemies;
+
+  constructor(enemies, health, damage) {
+    this.enemies = enemies;
+    this.health = health;
+    this.damage = damage;
+  }
+
+  updateEnemies() {
+
+  }
+
+  #damaged() {
+
+  }
+}
